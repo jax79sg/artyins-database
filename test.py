1a22dc37a897fe811914c0d098e0eb8c5c464fe9
jax79sg <jax79sg@yahoo.com.sg>
2020-01-06 16:00:37 +0800

    bug fix

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestConnections(unittest.TestCase):
         print("Waiting for mySQL to finish booting")
         time.sleep(15)
         import mysql.connector as mysql
-        db = mysql.connect(
+        self.db = mysql.connect(
            host = "127.0.0.1",
            database = "reportdb",
            user = "user",
@@ -16,12 +16,12 @@ class TestConnections(unittest.TestCase):
         
     def test_connectdb(self):
         print("Running ConnectDB test")
-        print(db)
+        print(self.db)
 
     def test_readdata(self):
         print("Running Read Data")
         print("Checking reports table")
-        cursor = db.cursor()
+        cursor = self.db.cursor()
         cursor.execute("select * from reports")
         records = cursor.fetchall()
         assert cursor.rowcount>=1,"There should be one or more reports records"
@@ -32,9 +32,9 @@ class TestConnections(unittest.TestCase):
 
     def test_writedata(self):
         print("Running Write Data")
-        cursor = db.cursor()
+        cursor = self.db.cursor()
         cursor.execute("INSERT INTO reports (filename,created_at,ingested_at,currentloc) VALUES (%s, %s, %s, %s)",("abc.pdf","2020-01-06 15:55:55","2020-01-06 16:33:33","/mnt/raw/reports/"))
-        db.commit()
+        self.db.commit()
         assert cursor.rowcount>=1,"There should be one report inserted"
         cursor.execute("INSERT INTO ingests (text,section,created_at,ingest_id,predicted_category,annotated_category) VALUES (%s, %s, %s, %s,%s,%s)",("This is the weather for singapore","observation","2020-01-06 15:55:55","1","DOCTRINE","DOCTRINE")) 
         cursor.close()
