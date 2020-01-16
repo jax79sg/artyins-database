1c32f5559d0f131c4a67d4d11e2dce53f024a801
jax79sg <jax79sg@yahoo.com.sg>
2020-01-16 15:32:19 +0800

    upadte

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE `reports` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `filename` varchar(255),
+  #`id` int PRIMARY KEY AUTO_INCREMENT,
+  `filename` varchar(255) PRIMARY KEY,
   `created_at` varchar(14),
   `ingested_at` varchar(14),
   `currentloc` varchar(255)
@@ -9,7 +9,7 @@ CREATE TABLE `reports` (
 
 CREATE TABLE `ingests` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `text` varchar(255),
+  `text` text,
   `section` varchar(255),
   `created_at` varchar(14),
   `ingest_id` varchar(1000),
@@ -18,7 +18,7 @@ CREATE TABLE `ingests` (
 );
 
 ALTER TABLE `ingests` ADD FOREIGN KEY (`ingest_id`) REFERENCES `reports` (`filename`);
-
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' identified by 'password';
 INSERT INTO reports (filename,created_at,ingested_at,currentloc) VALUES ("hello.pdf","20191231121212","20191231121312","/home/user/reports/raw/");
 INSERT INTO ingests (text, section, created_at, ingest_id, predicted_category, annotated_category) VALUES ('Hellow hellow','observation','20200121121313','hello.pdf','PERSONNEL','PERSONNEL');
 
